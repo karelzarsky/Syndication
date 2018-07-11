868f7c712c93b68b52a5a48ced2320b83e11aba8
Karel <karel.zarsky@gmail.com>
2018-07-11 21:39:18 +0200

    ML training tables

---
+++ SyndicateLogic/Entities/TrainSchema.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SyndicateLogic.Entities
+{
+	[Table("nn.trainSchema")]
+	public class TrainSchema
+	{
+		[Column(Order = 0), Key]
+		public int NetworkID { get; set; }
+		[Column(Order = 1), Key]
+		public int NeuronID { get; set; }
+		public bool Input { get; set; }
+		public int? ShingleID {get; set;}
+		public int? ForecastDays {get; set;}
+    }
+}
---
+++ SyndicateLogic/Entities/TrainValue.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SyndicateLogic.Entities
+{
+    [Table("nn.trainValue")]
+    public class TrainValue
+    {
+		[Column(Order = 0), Key]
+		public int NetworkID { get; set; }
+		[Column(Order = 1), Key]
+		public int NeuronID { get; set; }
+		[Column(Order = 2), Key]
+		public int ArticleID { get; set; }
+		public float Value { get; set; } = 0.0f;
+    }
+}
